0815b411b0370fb83ee66f889185c78d3dd5dfcc
p1gd0g <renhao.szu@gmail.com>
2022-07-03 03:36:01 +0800

    [feat] v0.0.2

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System;
 
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
     public Text _text = null;
     public Image _img = null;
+    public Button _btn = null;
+
+    DateTime _dateTime = new DateTime();
 
     // Start is called before the first frame update
     void Start()
     {
-        _text.text = "I love zoe";
-        _text.transform.DOJump(new Vector3(111, 111, 111), 3, 33, 3f);
+        _dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        _dateTime = _dateTime.AddSeconds(1638207420).ToLocalTime();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("hello zoe");
+        var ts = DateTime.Now.Subtract(_dateTime);
+        _text.text = string.Format("{0:%d} d, {0:%h} h, {0:%s} s", ts);
     }
 }
